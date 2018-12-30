6bc98fa2fc06393eb75087934ae24f6bef3269d5
TumoM <tumomasire@gmail.com>
2018-12-30 14:30:01 +0200

    Fixes

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const {ObjectID} = require('mongodb')
 
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/Todo');
-var {User} = require('./models/User');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 const port = (process.env.PORT || 3000);
