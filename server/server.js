5fdee4783a07ae57e3de09318459d51bf60e7707
TumoM <tumomasire@gmail.com>
2018-12-30 13:30:15 +0200

    Some tests

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb')
+
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
@@ -27,9 +29,27 @@ app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
-    res.status(400).send(e)
+    res.status(400);
   })
 });
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (ObjectID.isValid(id)){
+    Todo.findById(id, (err, query) => {
+      if (err){return res.status(400).send()}
+      else{
+        if (!query){ return res.status(404).send()}
+        else(res.send(query))
+      }
+    })
+  }else {
+    return res.status(404).send();
+  }
+},(err) => {
+  res.status(400).send(err)
+  console.log("Could not fetch", err);
+})
 
 app.listen(3000, () => {
   console.log("Server Running on: 3000");
