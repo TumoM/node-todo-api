d0941c33cdfb75b9c9f8bcfcee2f99a1a618afbe
TumoM <tumomasire@gmail.com>
2019-01-09 02:35:43 +0200

    User password now hashed before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+
+//
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+//
+// bcrypt.compare(password, hashedPassword, (err, res) => {
+//   console.log(res);
+// });
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -61,7 +73,7 @@ UserSchema.statics.findByToken = function(token){
     decoded = jwt.verify(token, "abc123");
   } catch (e) {
     return Promise.reject();
-    
+
   }
   return User.findOne({
     _id: decoded._id,
@@ -69,6 +81,19 @@ UserSchema.statics.findByToken = function(token){
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.pre('save', function(next){
+  var user = this;
+
+  if (user.isModified('password')){
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {next();}
+});
 
 var User = mongoose.model('User', UserSchema);
 
