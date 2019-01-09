a9c03d813bb69c9a944fe7e1761eba3e8093d596
TumoM <tumomasire@gmail.com>
2019-01-09 03:18:12 +0200

    Added logout route

--- server/server.js
+++ server/server.js
@@ -131,6 +131,15 @@ app.get("/users/me", authenticate,(req, res) => {
      res.status(400).send();
    });
  });
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  })
+});
+
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
